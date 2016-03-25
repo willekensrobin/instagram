ef4adadc5f8048d033ed23475f2a3283d3d2ec4f
williamverbiest <william.verbiest@gmail.com>
2016-03-25 06:14:08 +0100

    edit user.class

--- classes/user.class.php
+++ classes/user.class.php
@@ -8,6 +8,43 @@ var $m_sUsername;
 protected $m_sEmail;
 protected $m_hpassword;
 
+public function __set($p_sProperty, $p_sValue) {
+    switch($p_sProperty)
+    {
+    case "Firstname":
+    $this->m_sFirstname = $p_sValue;
+    break;
+
+    case "Lastname":
+    $this->m_sLastname = $p_sValue;
+    break;
+
+    case "Username":
+    $this->m_sUsername = $p_sValue;
+    break;
+
+    case "Email":
+        if (filter_var($p_sValue, FILTER_VALIDATE_EMAIL)) {
+            $this->m_sEmail = $p_sValue;
+        }
+        else
+        {
+            echo "Dit is geen geldog email adres"
+        }
+    $this->m_sEmail = $p_sValue;
+    break;
+    }
+}
+
+
+
+
+ function get_name() {
+
+ return $this->name;
+
+ }
+
     public function __construct($p_sFirstname, $m_sLastname, $p_nLeeftijd, $p_sGeboorteplaats) {
         $this->m_sFirstname= $p_sFirstname;
         $this->m_sLastname= $p_sLastname;
